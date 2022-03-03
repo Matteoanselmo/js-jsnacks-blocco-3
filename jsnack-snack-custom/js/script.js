4bf741c97b688401c90700023786405a4a61a230
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-03 12:11:42 +0100

    jsnack 3 custom

---
+++ jsnack-snack-custom/js/script.js
@@ -0,0 +1,18 @@
+const arrayNumber = [1 , 2 , 3 , 4 , 5 , 6 , 7];
+
+function oddOrEven(arrayInex){
+    if(arrayInex % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+for(let i = 0; i < arrayNumber.length; i++){
+    if(oddOrEven(arrayNumber[i])){
+        document.getElementById('rosso').innerHTML += arrayNumber[i];
+    }else{
+        document.getElementById('verde').innerHTML += arrayNumber[i];
+    }
+    
+}
